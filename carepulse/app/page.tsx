09de853a23d1ae0729a1a0c8c58838c87db5c742
Feedy <fedi.bn.mahfoudh@gmail.com>
2024-11-25 16:47:33 +0100

    home page commit

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className= "text-white text-3xl underline">
-        this is the home 
+        this is the home 123
       </h1>
       <Button>click me</Button>
     </div>
