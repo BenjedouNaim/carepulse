fad96887fc7e83e8b849455b4d4bb164931f080b
FedyBenMahfoudh <fedy.ben.mahfoudh@gmail.com>
2024-11-25 17:12:16 +0100

    333

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className= "text-white text-3xl underline">
-        this is the home 123
+        this is the home 123333333
       </h1>
       <Button>click me</Button>
     </div>
